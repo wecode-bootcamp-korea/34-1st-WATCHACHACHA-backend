220afdb8b270e087176ec232dc46629316a4406f
dev-taekwonlee <dev.taekwonlee@gmail.com>
2022-06-28 11:44:06 +0900

    Add: FilmView using query parameter (films/views.py)

--- films/urls.py
+++ films/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from films.views import FilmMainView
+from films.views import FilmView
 
 urlpatterns = [
-    path('/main', FilmMainView.as_view()),
+    path('', FilmView.as_view()),
 ]
--- films/views.py
+++ films/views.py
@@ -1,25 +1,41 @@
-import json
-
 from django.http      import JsonResponse
 from django.views     import View
-from django.db.models import Avg
 
-from films.models import Film
-from users.models import Rate
-from core.utils   import token_decorator
+from films.models     import Film
 
-class FilmMainView(View):
+class FilmView(View):
     def get(self, request):
+        # --- 국가 별 query string ---
+        # localhost:8000/films?country=한국                                  # 1. 한국 / 2. 미국
+        # localhost:8000/films?country=미국
         try:
-            film_data = [{
-                'id'            : film.id,
-                'name'          : film.name,
-                'release_date'  : film.release_date,
-                'image_url'     : film.image_url,
-                'country'       : [country.name for country in film.countries.all()],
-                'rating_system' : film.rating_system.rate,
-                'score_average' : film.rate_set.aggregate(Avg('score'))['score__avg'],
-            } for film in Film.objects.order_by('release_date')[:10]]
-            return JsonResponse({'film_data': film_data}, status=201)
-        except Film.DoesNotExist:
-            return JsonResponse({'message': 'FILM_DOES_NOT_EXIST'}, status=400)
+            country          = request.GET.get('country')
+            country_of_films = Film.objects.filter(countries__name=country)
+            results = [{
+                'id'               : film.id,
+                'name'             : film.name,
+                'release_date'     : film.release_date.year,
+                'image_url'        : film.image_url,
+                'country'          : [country.name for country in film.countries.all()],
+                'running_time_min' : film.running_time_min,
+            } for film in country_of_films[:10]]
+            return JsonResponse({'results': results}, status=201)
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'})
+
+        # --- 장르 별 query string ---
+        # localhost:8000/films?genre=드라마
+        # localhost:8000/films?genre=로맨스
+
+            # genre          = request.GET.get('genre')                           # 1. 드라마 / 2. 로맨스
+            # genre_of_films = Film.objects.filter(genres__name=genre)
+            # results = [{
+            #     'id'               : film.id,
+            #     'name'             : film.name,
+            #     'release_date'     : film.release_date.year,
+            #     'image_url'        : film.image_url,
+            #     'country'          : [country.name for country in film.countries.all()],
+            #     'running_time_min' : film.running_time_min,
+            # } for film in genre_of_films[:10]]
+            # return JsonResponse({'results': results}, status=201)

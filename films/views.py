c7f84f416dfb0156e2a68f83c088514e713ff624
dev-taekwonlee <dev.taekwonlee@gmail.com>
2022-06-28 16:38:41 +0900

    Update: FilmView (films/views.py)

--- films/views.py
+++ films/views.py
@@ -5,37 +5,41 @@ from films.models     import Film
 
 class FilmView(View):
     def get(self, request):
-        # --- 국가 별 query string ---
-        # localhost:8000/films?country=한국                                  # 1. 한국 / 2. 미국
-        # localhost:8000/films?country=미국
         try:
+            genre            = request.GET.get('genre')
             country          = request.GET.get('country')
-            country_of_films = Film.objects.filter(countries__name=country)
-            results = [{
-                'id'               : film.id,
-                'name'             : film.name,
-                'release_date'     : film.release_date.year,
-                'image_url'        : film.image_url,
-                'country'          : [country.name for country in film.countries.all()],
-                'running_time_min' : film.running_time_min,
-            } for film in country_of_films[:10]]
-            return JsonResponse({'results': results}, status=201)
+            running_time_min = int(request.GET.get('time'))
 
+            genre_of_films    = Film.objects.filter(genres__name = genre)
+            country_of_films  = Film.objects.filter(countries__name = country)
+            min_below_hundred = Film.objects.filter(running_time_min__lte = running_time_min)
+
+            results = {
+                'drama' : [{
+                    'id'               : film.id,
+                    'name'             : film.name,
+                    'release_date'     : film.release_date.year,
+                    'image_url'        : film.image_url,
+                    'country'          : [country.name for country in film.countries.all()],
+                    'running_time_min' : film.running_time_min,
+                } for film in genre_of_films[:10]],
+                'america' : [{
+                    'id'               : film.id,
+                    'name'             : film.name,
+                    'release_date'     : film.release_date.year,
+                    'image_url'        : film.image_url,
+                    'country'          : [country.name for country in film.countries.all()],
+                    'running_time_min' : film.running_time_min,
+                } for film in country_of_films[:10]],
+                'running_time_below_hundred' : [{
+                    'id'               : film.id,
+                    'name'             : film.name,
+                    'release_date'     : film.release_date.year,
+                    'image_url'        : film.image_url,
+                    'country'          : [country.name for country in film.countries.all()],
+                    'running_time_min' : film.running_time_min,
+                } for film in min_below_hundred[:10]]
+            }
+            return JsonResponse({'results': results}, status = 201)
         except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR'})
-
-        # --- 장르 별 query string ---
-        # localhost:8000/films?genre=드라마
-        # localhost:8000/films?genre=로맨스
-
-            # genre          = request.GET.get('genre')                           # 1. 드라마 / 2. 로맨스
-            # genre_of_films = Film.objects.filter(genres__name=genre)
-            # results = [{
-            #     'id'               : film.id,
-            #     'name'             : film.name,
-            #     'release_date'     : film.release_date.year,
-            #     'image_url'        : film.image_url,
-            #     'country'          : [country.name for country in film.countries.all()],
-            #     'running_time_min' : film.running_time_min,
-            # } for film in genre_of_films[:10]]
-            # return JsonResponse({'results': results}, status=201)
+            return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
